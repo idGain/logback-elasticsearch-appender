fb28dbd5d15bf0d9e5ed715a503ba6376b6642ec
Carsten Götzinger <carsten.goetzinger@concedro.com>
2020-06-22 16:58:42 +0200

    prepare for release build

--- travis/release.sh
+++ travis/release.sh
@@ -3,4 +3,4 @@
 set -e
 
 echo "Create release"
-./mvnw initialize release:clean release:prepare release:perform --settings ./travis/settings.xml -DreleaseVersion=$TRAVIS_TAG -DskipTests=true --batch-mode --update-snapshots -Prelease
+./mvnw initialize release:clean release:prepare release:perform --settings ./travis/settings.xml -DreleaseVersion=$TRAVIS_TAG -DskipTests=true -DcheckModificationExcludeList=travis/*.sh --batch-mode --update-snapshots -Prelease
