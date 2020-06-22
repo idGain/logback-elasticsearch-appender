3db20cef677d63481f83463baaf33b4cb1249e4c
Carsten Götzinger <carsten.goetzinger@concedro.com>
2020-06-22 16:45:30 +0200

    prepare for release build

--- travis/before-deploy.sh
+++ travis/before-deploy.sh
@@ -10,12 +10,16 @@ declare -r secring_auto="${GPG_DIR}/secring.auto"
 declare -r pubring_auto="${GPG_DIR}/pubring.auto"
 
 echo
-echo "Decrypting secret gpg keyring.."
-{ echo $GPG_PASSWORD | gpg --passphrase-fd 0 "${secring_auto}".gpg ; } || { end_with_error "Failed to decrypt secret gpg keyring." ; }
-echo Success!
+echo "Import signer public key..."
+gpg ---home "${GPG_DIR}" --import "${GPG_DIR}/signer.pub.asc"
 
 echo
-echo Importing keyrings..
+echo "Decrypting secret gpg keyring.."
+{ echo $GPG_PASSWORD | gpg --passphrase-fd 0 "${secring_auto}".gpg ; } || { end_with_error "Failed to decrypt secret gpg keyring." ; }
+echo "Success!"
+
+echo
+echo "Importing keyrings.."
 { gpg --home "${GPG_DIR}" --import "${secring_auto}" ; } || { end_with_error "Could not import secret keyring into gpg." ; }
 { gpg --home "${GPG_DIR}" --import "${pubring_auto}" ; } || { end_with_error "Could not import public keyring into gpg." ; }
 echo Success!
