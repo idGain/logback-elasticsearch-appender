cd062020d8c31afee1c0acd715f16fd44ba72a7f
cgoIT <info@cgo-it.de>
2020-06-24 10:50:39 +0200

    [skip ci] [maven-release-plugin] Integrate changes from release 1.8-SNAPSHOT (#5)
    * Release 1.7
    
    * Release 1.7
    
    * Release 1.7
    
    * Release 1.7
    
    * Release 1.7
    
    * Release 1.7
    
    * Release 1.7
    
    * Release 1.7
    
    * Release 1.7
    
    * [skip ci] [maven-release-plugin] prepare release v1.7
    
    * [skip ci] [maven-release-plugin] prepare for next development iteration
    
    * Release 1.7
    
    * [skip ci] [maven-release-plugin] prepare release v1.7
    
    * [skip ci] [maven-release-plugin] prepare for next development iteration
    
    * Release 1.7
    
    * [skip ci] [maven-release-plugin] prepare release v1.7
    
    * [skip ci] [maven-release-plugin] prepare for next development iteration
    
    * Release 1.7
    
    * Release 1.7
    
    * [skip ci] [maven-release-plugin] prepare release v1.7
    
    * [skip ci] [maven-release-plugin] prepare for next development iteration
    
    * [skip ci] [maven-release-plugin] prepare release v1.8
    
    * [skip ci] [maven-release-plugin] prepare for next development iteration
    
    * Release 1.7
    
    * [skip ci] [maven-release-plugin] prepare release v1.7
    
    * [skip ci] [maven-release-plugin] prepare for next development iteration
    
    * Release 1.7
    
    * Release 1.7
    
    * [skip ci] [maven-release-plugin] prepare release v1.7
    
    * [skip ci] [maven-release-plugin] prepare for next development iteration
    
    * Release 1.7
    
    * [skip ci] [maven-release-plugin] prepare release v1.7
    
    * [skip ci] [maven-release-plugin] prepare for next development iteration
    
    * [skip ci] [maven-release-plugin] rollback the release of v1.7
    
    * Release 1.7
    
    * [skip ci] [maven-release-plugin] prepare release v1.7
    
    * [skip ci] [maven-release-plugin] prepare for next development iteration
    
    * [skip ci] [maven-release-plugin] rollback the release of v1.7
    
    * Release 1.7
    
    * [skip ci] [maven-release-plugin] prepare release v1.7
    
    * [skip ci] [maven-release-plugin] prepare for next development iteration
    
    * [skip ci] [maven-release-plugin] rollback the release of v1.7
    
    * Release 1.7
    
    * [skip ci] [maven-release-plugin] prepare release v1.7
    
    * [skip ci] [maven-release-plugin] rollback the release of v1.7
    
    * [skip ci] [maven-release-plugin] prepare release v1.7
    
    * [skip ci] [maven-release-plugin] prepare for next development iteration
    
    Co-authored-by: Carsten Götzinger <carsten@cgo-it.de>

--- .travis/build.sh
+++ .travis/build.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env bash
 
-# Stop on error
-set -e
-
 main() {
     init
     setup_git
@@ -34,26 +31,86 @@ setup_git() {
     git config --global user.name "Carsten Götzinger"
 }
 
-pushTagsAndCommit() {
-    exeinf "Pushing tags"
-    git push --tags
-    exeinf "Pushing maven commit"
-    git push -u origin release
+fix_git() {
+    git checkout ${TRAVIS_BRANCH}
+    git branch -u origin/${TRAVIS_BRANCH}
+    git config branch.${TRAVIS_BRANCH}.remote origin
+    git config branch.${TRAVIS_BRANCH}.merge refs/heads/${TRAVIS_BRANCH}
+}
+
+provide_gpg_keys() {
+  rm -rf ${GPG_DIR} && mkdir -p ${GPG_DIR}
+  openssl aes-256-cbc -K $encrypted_f3cb6cb238ed_key -iv $encrypted_f3cb6cb238ed_iv -in deploy/signingkey.asc.enc -out ${GPG_DIR}/signingkey.asc -d
+  gpg2 --keyring=${GPG_DIR}/pubring.gpg --no-default-keyring --import ${GPG_DIR}/signingkey.asc
+  gpg2 --allow-secret-key-import --keyring=${GPG_DIR}/secring.gpg --no-default-keyring  --import ${GPG_DIR}/signingkey.asc
+
+  gpg2 --list-keys
+  gpg2 --list-secret-keys
+}
+
+get_version() {
+  mvn org.apache.maven.plugins:maven-help-plugin:evaluate -Dexpression=project.version -q -DforceStdout
+}
+
+delete_tag() {
+  local version="$1"
+  git push --delete origin v${version}
+}
+
+create_pull_request() {
+  local version="$1"
+  payload=$( cat <<- EOP
+        {
+          "title": "[skip ci] [maven-release-plugin] Integrate changes from release ${version}",
+          "body": "Please pull these changes from release build!",
+          "head": "release",
+          "base": "master"
+        }
+EOP
+)
+  curl -s -H "Authorization: token ${GITHUB_TOKEN}" -H "Accept: application/vnd.github.v3+json" -d "${payload}" https://api.github.com/repos/cgoIT/logback-elasticsearch-appender/pulls
+}
+
+on_failure() {
+    exeinf "Rollback release"
+
+    mvn -B -s .travis/settings.xml release:rollback -DscmCommentPrefix="[skip ci] [maven-release-plugin] " -DcheckModificationExcludeList=.travis/*.sh -Prelease
+}
+
+on_success() {
+    exeinf 'Create pull request'
+
+    version="$(get_version)"
+    create_pull_request "${version}"
 }
 
 buildArtifact() {
     echo "Branch is ${BRANCH_NAME}"
 
-    if [[ $TRAVIS_BRANCH == "release" ]]; then
+    if [[ "$TRAVIS_BRANCH" == "release" && "$TRAVIS_PULL_REQUEST" == "false" ]]; then
         exeinf "Release build"
 
-        exeinf "Performing maven release"
-        ./mvnw -B -s .travis/settings.xml release:clean release:prepare release:perform -DscmCommentPrefix="[skip ci] [maven-release-plugin] " -DcheckModificationExcludeList=.travis/*.sh
+        exeinf "Fix git checkout"
+        fix_git
 
-        pushTagsAndCommit
+        exeinf "Provide gpg keys"
+        provide_gpg_keys
+
+        exeinf "Performing maven release"
+        mvn -B -s .travis/settings.xml clean initialize release:prepare release:perform -DscmCommentPrefix="[skip ci] [maven-release-plugin] " -DcheckModificationExcludeList=.travis/*.sh -Prelease
+        rc="$?"
+
+        if [[ "$rc" -eq 0 ]] ; then
+          exeinf 'Release build successful.'
+          on_success
+        else
+          exeerr 'Release build not successful. Rollback...';
+          on_failure
+          exit $rc
+        fi
     else
-        exeinf "Travis Snapshot build"
-        ./mvnw -s .travis/settings.xml package -Dgpg.skip
+        exeinf "Travis snapshot build"
+        mvn -s .travis/settings.xml package -Dgpg.skip
     fi
 }
 
