06f70534a61dd63296aabf1a0d5a3ceebcb392fe
Ahmet Kaplan <kaplan38@gmail.com>
2022-04-15 12:31:24 +0300

    MockitoJUnitRunner placed

--- src/test/java/de/cgoit/logback/elasticsearch/PropertySerializerTest.java
+++ src/test/java/de/cgoit/logback/elasticsearch/PropertySerializerTest.java
@@ -8,7 +8,7 @@ import de.cgoit.logback.elasticsearch.util.ClassicPropertyAndEncoder;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
-import org.mockito.runners.MockitoJUnitRunner;
+import org.mockito.junit.MockitoJUnitRunner;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
